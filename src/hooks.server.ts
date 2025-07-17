f916fdc6aa875b21ab1045319f496dd3f069a714
S. Elliott Johnson <sejohnson@torchcloudconsulting.com>
2025-07-16 21:37:53 -0600

    bigger handle

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,4 +1,10 @@
-export function handle({ event, resolve }) {
-	console.log('handling');
-	return resolve(event);
-}
+import { sequence } from '@sveltejs/kit/hooks';
+
+export const handle = sequence(
+	({ event, resolve }) =>
+		new Promise((resolve) => setTimeout(resolve, 1000)).then(() => resolve(event)),
+	async ({ event, resolve }) => {
+		await new Promise((resolve) => setTimeout(resolve, 2000));
+		return resolve(event);
+	}
+);
